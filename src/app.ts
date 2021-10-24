efd474e4689cf0b39f610e607644579ea67336d5
Renan Silva <renan.silva@ext.hdi.com.br>
2021-10-24 15:37:47 -0300

    feat: add Joi validation on ParentService

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import Joi from 'joi';
 import AppError from './errors/AppError';
 import router from './routes';
 
@@ -8,6 +9,11 @@ const PORT = process.env.SERVER_PORT || 8080;
 const API_BASE_URL = process.env.SERVER_BASE_URL || '/api/v1';
 
 const app = express();
+
+interface FieldErrorDetail {
+  field: string;
+  description: string;
+}
 
 app.use(express.json());
 app.use(API_BASE_URL, router);
@@ -17,6 +23,21 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
+    });
+  }
+
+  if (err instanceof Joi.ValidationError) {
+    console.error('Joi validatin error ');
+    return response.status(400).json({
+      status: 'error',
+      message: 'Invalid input data, check the errors object for more information',
+      errors: err.details.map((error) => {
+        const fieldErrorDetails = {
+          field: error.context.key,
+          description: error.message.replace(/[^\w\s]/gi, ''),
+        };
+        return fieldErrorDetails;
+      }),
     });
   }
 
--- src/services/ParentService.ts
+++ src/services/ParentService.ts
@@ -1,9 +1,21 @@
+import Joi from 'joi';
 import CreateParentRequestDTO from '../dtos/CreateParentRequestDTO';
 import AppError from '../errors/AppError';
 import prismaClient from '../prisma';
 
 class ParentService {
   async createParent(payload: CreateParentRequestDTO) {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      surname: Joi.string().required(),
+      nickname: Joi.string(),
+    });
+
+    await schema.validateAsync(payload, {
+      abortEarly: false,
+    });
+
     console.log(`PaymentService.createParent \nData: ${JSON.stringify(payload)}`);
     try {
       const created = await prismaClient.parent.create({
