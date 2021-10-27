c71a3dffc313da8bd00945d5a4c2e735d8fd34d5
Renan Silva <renan.silva@ext.hdi.com.br>
2021-10-27 19:53:59 -0300

    feat: add schema validation on createQuest

--- backend/src/services/QuestService.ts
+++ backend/src/services/QuestService.ts
@@ -1,8 +1,17 @@
 import { PrismaClient } from '.prisma/client';
+import Joi from 'joi';
 import CreateQuestRequestDTO from '../dtos/CreateQuestRequestDTO';
 
 class QuestService {
   async createQuest(parentId: number, quest: CreateQuestRequestDTO) {
+    const schema = Joi.object({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      stars: Joi.number().integer().min(1).max(5),
+    });
+
+    await schema.validateAsync(quest);
+
     const prismaClient = new PrismaClient();
 
     const createdQuest = await prismaClient.quest.create({
