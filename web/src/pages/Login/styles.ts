b2afc28f3a74ff14c4493510df773013e79ff111
Renan Silva <renan.silva@ext.hdi.com.br>
2021-12-11 18:21:41 -0300

    fix: overflow on Login page

--- web/src/pages/Login/styles.ts
+++ web/src/pages/Login/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   overflow: hidden;
   width: 100%;
+  height: 500px;
   min-height: 100vh;
   display: -webkit-box;
   display: -webkit-flex;
